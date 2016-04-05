938dc21621ee7b8d65c7286034af8a04b1f59b13
Jon <jonthughes@hotmail.com>
2016-04-05 09:55:17 -0700

    Added yet another activity

---
+++ app/src/main/java/com/example/david/firstapplication/YetAnotherActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YetAnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yet_another);
+    }
+}
